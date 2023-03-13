a74fe818c3d724bcffe75c87e727ca0a02ca41c4
viridityzhu <zhujiayin@u.nus.edu>
2023-03-13 16:19:04 +0800

    update eval.py to support: 1. evaluate on training set; 2. specify gt_path and pred_path independently; 3. specify whether to eval verts;

--- eval.py
+++ eval.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import pip
 import argparse
 import json
+import base64
 
 def install(package):
     if hasattr(pip, 'main'):
@@ -112,21 +113,26 @@ class curve:
 
 def createHTML(outputDir, curve_list):
     curve_data_list = list()
-    for item in curve_list:
+    for idx, item in enumerate(curve_list):
         fig1 = plt.figure()
         ax = fig1.add_subplot(111)
         ax.plot(item.x_data, item.y_data)
         ax.set_xlabel(item.x_label)
         ax.set_ylabel(item.y_label)
-        img_path = os.path.join(outputDir, "img_path_path.png")
+        img_path = os.path.join(outputDir, f"curve_{idx}.png")
         plt.savefig(img_path, bbox_inches=0, dpi=300)
 
         # write image and create html embedding
-        data_uri1 = open(img_path, 'rb').read().encode('base64').replace('\n', '')
+        # data_uri1 = open(img_path, 'rb').read().encode('base64').replace('\n', '')
+        with open(img_path, 'rb') as f:
+            img_bytes = f.read()
+            img_b64 = base64.b64encode(img_bytes)
+            data_uri1 = img_b64.decode('utf-8')
+            data_uri1 = data_uri1.replace('\n', '')
         img_tag1 = 'src="data:image/png;base64,{0}"'.format(data_uri1)
         curve_data_list.append((item.text, img_tag1))
 
-        os.remove(img_path)
+        # os.remove(img_path)
 
     htmlString = '''<!DOCTYPE html>
     <html>
@@ -176,7 +182,7 @@ def _search_pred_file(pred_path, pred_file_name):
         raise Exception('Giving up, because its not clear which file to evaluate.')
 
 
-def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
+def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None, eval_verts=True):
     if pred_file_name is None:
         pred_file_name = 'pred.json'
     if set_name is None:
@@ -193,11 +199,13 @@ def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
 
     assert len(pred) == 2, 'Expected format mismatch.'
     assert len(pred[0]) == len(xyz_list), 'Expected format mismatch.'
-    assert len(pred[1]) == len(xyz_list), 'Expected format mismatch.'
+    if eval_verts:
+        assert len(pred[1]) == len(xyz_list), 'Expected format mismatch.'
 
     # init eval utils
     eval_xyz, eval_xyz_aligned = EvalUtil(), EvalUtil()
-    eval_mesh_err, eval_mesh_err_aligned = EvalUtil(num_kp=778), EvalUtil(num_kp=778)
+    if eval_verts:
+        eval_mesh_err, eval_mesh_err_aligned = EvalUtil(num_kp=778), EvalUtil(num_kp=778)
     f_score, f_score_aligned = list(), list()
     f_threshs = [0.005, 0.015]
 
@@ -217,8 +225,11 @@ def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
         xyz, verts = xyz_list[idx], verts_list[idx]
         xyz, verts = [np.array(x) for x in [xyz, verts]]
 
-        xyz_pred, verts_pred = pred[0][idx], pred[1][idx]
-        xyz_pred, verts_pred = [np.array(x) for x in [xyz_pred, verts_pred]]
+        xyz_pred = pred[0][idx]
+        xyz_pred = np.array(xyz_pred)
+        if eval_verts:
+            verts_pred = pred[1][idx]
+            verts_pred = np.array(verts_pred)
 
         # Not aligned errors
         eval_xyz.feed(
@@ -227,27 +238,29 @@ def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
             xyz_pred
         )
 
-        if shape_is_mano is None:
-            if verts_pred.shape[0] == verts.shape[0]:
-                shape_is_mano = True
-            else:
-                shape_is_mano = False
+        if eval_verts:
+            if shape_is_mano is None:
+                if verts_pred.shape[0] == verts.shape[0]:
+                    shape_is_mano = True
+                else:
+                    shape_is_mano = False
 
-        if shape_is_mano:
-            eval_mesh_err.feed(
-                verts,
-                np.ones_like(verts[:, 0]),
-                verts_pred
-            )
+            if shape_is_mano:
+                eval_mesh_err.feed(
+                    verts,
+                    np.ones_like(verts[:, 0]),
+                    verts_pred
+                )
 
         # align predictions
         xyz_pred_aligned = align_w_scale(xyz, xyz_pred)
-        if shape_is_mano:
-            verts_pred_aligned = align_w_scale(verts, verts_pred)
-        else:
-            # use trafo estimated from keypoints
-            trafo = align_w_scale(xyz, xyz_pred, return_trafo=True)
-            verts_pred_aligned = align_by_trafo(verts_pred, trafo)
+        if eval_verts:
+            if shape_is_mano:
+                verts_pred_aligned = align_w_scale(verts, verts_pred)
+            else:
+                # use trafo estimated from keypoints
+                trafo = align_w_scale(xyz, xyz_pred, return_trafo=True)
+                verts_pred_aligned = align_by_trafo(verts_pred, trafo)
 
         # Aligned errors
         eval_xyz_aligned.feed(
@@ -256,23 +269,24 @@ def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
             xyz_pred_aligned
         )
 
-        if shape_is_mano:
-            eval_mesh_err_aligned.feed(
-                verts,
-                np.ones_like(verts[:, 0]),
-                verts_pred_aligned
-            )
+        if eval_verts:
+            if shape_is_mano:
+                eval_mesh_err_aligned.feed(
+                    verts,
+                    np.ones_like(verts[:, 0]),
+                    verts_pred_aligned
+                )
 
-        # F-scores
-        l, la = list(), list()
-        for t in f_threshs:
-            # for each threshold calculate the f score and the f score of the aligned vertices
-            f, _, _ = calculate_fscore(verts, verts_pred, t)
-            l.append(f)
-            f, _, _ = calculate_fscore(verts, verts_pred_aligned, t)
-            la.append(f)
-        f_score.append(l)
-        f_score_aligned.append(la)
+            # F-scores
+            l, la = list(), list()
+            for t in f_threshs:
+                # for each threshold calculate the f score and the f score of the aligned vertices
+                f, _, _ = calculate_fscore(verts, verts_pred, t)
+                l.append(f)
+                f, _, _ = calculate_fscore(verts, verts_pred_aligned, t)
+                la.append(f)
+            f_score.append(l)
+            f_score_aligned.append(la)
 
     # Calculate results
     xyz_mean3d, _, xyz_auc3d, pck_xyz, thresh_xyz = eval_xyz.get_measures(0.0, 0.05, 100)
@@ -283,27 +297,28 @@ def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
     print('Evaluation 3D KP ALIGNED results:')
     print('auc=%.3f, mean_kp3d_avg=%.2f cm\n' % (xyz_al_auc3d, xyz_al_mean3d * 100.0))
 
-    if shape_is_mano:
-        mesh_mean3d, _, mesh_auc3d, pck_mesh, thresh_mesh = eval_mesh_err.get_measures(0.0, 0.05, 100)
-        print('Evaluation 3D MESH results:')
-        print('auc=%.3f, mean_kp3d_avg=%.2f cm' % (mesh_auc3d, mesh_mean3d * 100.0))
+    if eval_verts:
+        if shape_is_mano:
+            mesh_mean3d, _, mesh_auc3d, pck_mesh, thresh_mesh = eval_mesh_err.get_measures(0.0, 0.05, 100)
+            print('Evaluation 3D MESH results:')
+            print('auc=%.3f, mean_kp3d_avg=%.2f cm' % (mesh_auc3d, mesh_mean3d * 100.0))
 
-        mesh_al_mean3d, _, mesh_al_auc3d, pck_mesh_al, thresh_mesh_al = eval_mesh_err_aligned.get_measures(0.0, 0.05, 100)
-        print('Evaluation 3D MESH ALIGNED results:')
-        print('auc=%.3f, mean_kp3d_avg=%.2f cm\n' % (mesh_al_auc3d, mesh_al_mean3d * 100.0))
-    else:
-        mesh_mean3d, mesh_auc3d, mesh_al_mean3d, mesh_al_auc3d = -1.0, -1.0, -1.0, -1.0
+            mesh_al_mean3d, _, mesh_al_auc3d, pck_mesh_al, thresh_mesh_al = eval_mesh_err_aligned.get_measures(0.0, 0.05, 100)
+            print('Evaluation 3D MESH ALIGNED results:')
+            print('auc=%.3f, mean_kp3d_avg=%.2f cm\n' % (mesh_al_auc3d, mesh_al_mean3d * 100.0))
+        else:
+            mesh_mean3d, mesh_auc3d, mesh_al_mean3d, mesh_al_auc3d = -1.0, -1.0, -1.0, -1.0
 
-        pck_mesh, thresh_mesh = np.array([-1.0, -1.0]), np.array([0.0, 1.0])
-        pck_mesh_al, thresh_mesh_al = np.array([-1.0, -1.0]), np.array([0.0, 1.0])
+            pck_mesh, thresh_mesh = np.array([-1.0, -1.0]), np.array([0.0, 1.0])
+            pck_mesh_al, thresh_mesh_al = np.array([-1.0, -1.0]), np.array([0.0, 1.0])
 
-    print('F-scores')
-    f_out = list()
-    f_score, f_score_aligned = np.array(f_score).T, np.array(f_score_aligned).T
-    for f, fa, t in zip(f_score, f_score_aligned, f_threshs):
-        print('F@%.1fmm = %.3f' % (t*1000, f.mean()), '\tF_aligned@%.1fmm = %.3f' % (t*1000, fa.mean()))
-        f_out.append('f_score_%d: %f' % (round(t*1000), f.mean()))
-        f_out.append('f_al_score_%d: %f' % (round(t*1000), fa.mean()))
+        print('F-scores')
+        f_out = list()
+        f_score, f_score_aligned = np.array(f_score).T, np.array(f_score_aligned).T
+        for f, fa, t in zip(f_score, f_score_aligned, f_threshs):
+            print('F@%.1fmm = %.3f' % (t*1000, f.mean()), '\tF_aligned@%.1fmm = %.3f' % (t*1000, fa.mean()))
+            f_out.append('f_score_%d: %f' % (round(t*1000), f.mean()))
+            f_out.append('f_al_score_%d: %f' % (round(t*1000), fa.mean()))
 
     # Dump results
     score_path = os.path.join(output_dir, 'scores.txt')
@@ -315,38 +330,54 @@ def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
         fo.write('xyz_al_mean3d: %f\n' % xyz_al_mean3d)
         fo.write('xyz_al_auc3d: %f\n' % xyz_al_auc3d)
 
-        mesh_mean3d *= 100
-        mesh_al_mean3d *= 100
-        fo.write('mesh_mean3d: %f\n' % mesh_mean3d)
-        fo.write('mesh_auc3d: %f\n' % mesh_auc3d)
-        fo.write('mesh_al_mean3d: %f\n' % mesh_al_mean3d)
-        fo.write('mesh_al_auc3d: %f\n' % mesh_al_auc3d)
-        for t in f_out:
-            fo.write('%s\n' % t)
+        if eval_verts:
+            mesh_mean3d *= 100
+            mesh_al_mean3d *= 100
+            fo.write('mesh_mean3d: %f\n' % mesh_mean3d)
+            fo.write('mesh_auc3d: %f\n' % mesh_auc3d)
+            fo.write('mesh_al_mean3d: %f\n' % mesh_al_mean3d)
+            fo.write('mesh_al_auc3d: %f\n' % mesh_al_auc3d)
+            for t in f_out:
+                fo.write('%s\n' % t)
     print('Scores written to: %s' % score_path)
 
     # scale to cm
     thresh_xyz *= 100.0
     thresh_xyz_al *= 100.0
-    thresh_mesh *= 100.0
-    thresh_mesh_al *= 100.0
 
-    createHTML(
-        output_dir,
-        [
-            curve(thresh_xyz, pck_xyz, 'Distance in cm', 'Percentage of correct keypoints', 'PCK curve for keypoint error'),
-            curve(thresh_xyz_al, pck_xyz_al, 'Distance in cm', 'Percentage of correct keypoints', 'PCK curve for aligned keypoint error'),
-            curve(thresh_mesh, pck_mesh, 'Distance in cm', 'Percentage of correct vertices', 'PCV curve for mesh error'),
-            curve(thresh_mesh_al, pck_mesh_al, 'Distance in cm', 'Percentage of correct vertices', 'PCV curve for aligned mesh error')
-        ]
-    )
+    if eval_verts:
+        thresh_mesh *= 100.0
+        thresh_mesh_al *= 100.0
 
-    pck_curve_data = {
-        'xyz': [thresh_xyz.tolist(), pck_xyz.tolist()],
-        'xyz_al': [thresh_xyz_al.tolist(), pck_xyz_al.tolist()],
-        'mesh': [thresh_mesh.tolist(), pck_mesh.tolist()],
-        'mesh_al': [thresh_mesh_al.tolist(), pck_mesh_al.tolist()],
-    }
+    if eval_verts:
+        createHTML(
+            output_dir,
+            [
+                curve(thresh_xyz, pck_xyz, 'Distance in cm', 'Percentage of correct keypoints', 'PCK curve for keypoint error'),
+                curve(thresh_xyz_al, pck_xyz_al, 'Distance in cm', 'Percentage of correct keypoints', 'PCK curve for aligned keypoint error'),
+                curve(thresh_mesh, pck_mesh, 'Distance in cm', 'Percentage of correct vertices', 'PCV curve for mesh error'),
+                curve(thresh_mesh_al, pck_mesh_al, 'Distance in cm', 'Percentage of correct vertices', 'PCV curve for aligned mesh error')
+            ]
+        )
+        pck_curve_data = {
+            'xyz': [thresh_xyz.tolist(), pck_xyz.tolist()],
+            'xyz_al': [thresh_xyz_al.tolist(), pck_xyz_al.tolist()],
+            'mesh': [thresh_mesh.tolist(), pck_mesh.tolist()],
+            'mesh_al': [thresh_mesh_al.tolist(), pck_mesh_al.tolist()],
+        }
+    else:
+        createHTML(
+            output_dir,
+            [
+                curve(thresh_xyz, pck_xyz, 'Distance in cm', 'Percentage of correct keypoints', 'PCK curve for keypoint error'),
+                curve(thresh_xyz_al, pck_xyz_al, 'Distance in cm', 'Percentage of correct keypoints', 'PCK curve for aligned keypoint error'),
+            ]
+        )
+        pck_curve_data = {
+            'xyz': [thresh_xyz.tolist(), pck_xyz.tolist()],
+            'xyz_al': [thresh_xyz_al.tolist(), pck_xyz_al.tolist()],
+        }
+
     with open('pck_data.json', 'w') as fo:
         json.dump(pck_curve_data, fo)
 
@@ -355,19 +386,35 @@ def main(gt_path, pred_path, output_dir, pred_file_name=None, set_name=None):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Show some samples from the dataset.')
-    parser.add_argument('input_dir', type=str,
+    parser.add_argument('--input_dir', type=str,
                         help='Path to where prediction the submited result and the ground truth is.')
-    parser.add_argument('output_dir', type=str,
+    parser.add_argument('--output_dir', type=str, default='results',
                         help='Path to where the eval result should be.')
     parser.add_argument('--pred_file_name', type=str, default='pred.json',
                         help='Name of the eval file.')
+    parser.add_argument('--gt_path', type=str, default='/storage_fast/jyzhu/HandRecon/freihand', 
+                        help='Path to where the ground truth is.')
+    parser.add_argument('--pred_path', type=str, default=None,
+                        help='Path to where the submited result is.')
+    parser.add_argument('--eval_verts', dest='eval_verts', action=argparse.BooleanOptionalAction,
+                        help='Whether to eval verts. if not: --no-eval_verts')
+    parser.add_argument('--set_name', type=str, default='evaluation', 
+                        help='training set or evaluation set.')
+    parser.set_defaults(eval_verts=True)
     args = parser.parse_args()
+
+    # ** edited
+    gt_path = args.gt_path if args.gt_path is not None else os.path.join(args.input_dir, 'ref')
+    pred_path = args.pred_path if args.pred_path is not None else os.path.join(args.input_dir, 'res')
+    print(f'Evaluation verts: {args.eval_verts}')
+    print(f'Set name: {args.set_name}')
 
     # call eval
     main(
-        os.path.join(args.input_dir, 'ref'),
-        os.path.join(args.input_dir, 'res'),
+        gt_path,
+        pred_path,
         args.output_dir,
         args.pred_file_name,
-        set_name='evaluation'
+        set_name=args.set_name,
+        eval_verts=args.eval_verts,
     )
